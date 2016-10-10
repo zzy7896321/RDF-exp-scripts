43bfeb60f74dc7c4343b7616dd4da9739919db97
Zhuuoyue Zhao <zyzhao@cs.utah.edu>
2016-10-10 15:15:41 -0600

    minor bug fix

--- watdiv-dataset/gen_data.sh
+++ watdiv-dataset/gen_data.sh
@@ -9,7 +9,7 @@ rm -rf "$BASEDIR/scale_$1"
 mkdir "$BASEDIR/scale_$1"
 cd "$BASEDIR/scale_$1"
 
-hdfs dfs -rm -r watdiv/scale_$1
+hdfs dfs -rm -r -f watdiv/scale_$1
 hdfs dfs -mkdir -p watdiv/scale_$1
 "$WATDIV_HOME/bin/Release/watdiv" -d "$WATDIV_HOME/model/wsdbm-data-model-short-pred.txt" $1 | hdfs dfs -put - watdiv/scale_$1/raw_triplets.txt
 
